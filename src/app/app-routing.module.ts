04ee7de0abebb7c320989b6f8e3fc8ff58267a05
mohana22 <nirmaangit@gmail.com>
2021-03-11 12:23:15 +0530

    second modify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Home', component:HomeComponent},
+  {path:'Login', component:LoginComponent},
+  {path:'Registration', component:RegistrationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
